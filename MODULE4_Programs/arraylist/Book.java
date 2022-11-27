6e1e93f891dfeb5180eba6675e72a246104b39ec
sanjana-rohiny <119188245+sanjana-rohiny@users.noreply.github.com>
2022-11-27 21:40:18 +0530

    Delete main directory

---
+++ MODULE4_Programs/arraylist/Book.java
@@ -0,0 +1,23 @@
+
+public class Book {
+
+	int id;
+	String title;
+	String author;
+	/*String publisher;
+	String language;
+	String type;
+	String noOfpages; */
+	public Book(int id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	
+	public void printBookInfo() {
+		System.out.println("Book id = " + id);
+		System.out.println("Book name = " + title);
+		System.out.println("Book id = " + author);
+	}
+}
